3cc5aaae50fb49431ecde1a8ccc062328e958b2b
gdm-1718-indymeer <indymeer@student.arteveldehs.be>
2019-10-14 09:38:26 +0200

    init

---
+++ Lessen/Les3/oefening1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace oefening1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int getal;
+            getal = 5;
+
+          if(getal < 8){
+                 getal ++;
+                 Console.WriteLine("De waarde van de getal is: " + getal);
+
+            }else if (getal > 6 ){
+
+                 getal ++;
+                 Console.WriteLine("De waarde van de getal is: " + getal);
+
+            } else if (getal >= 7) {
+                
+                 Console.WriteLine("De waarde van de getal is: " + getal);
+
+            } 
+
+            
+    
+        }
+    }
+}
---
+++ Lessen/Les3/oefening2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace oefening2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Hello World!");
+           int a = 5;
+           int b = 6;
+           int resultaat1 =  optellen(a , b);
+           int resultaat2 = deling(a , b);
+           int resultaat3 = verhogen(a);
+        }
+        static int optellen(int a , int b){
+            return a + b; 
+        }
+        static int deling(int a , int b){
+            return a / b; 
+        }
+        static int verhogen(int a){
+            return a++ ; 
+        }
+    }
+}
