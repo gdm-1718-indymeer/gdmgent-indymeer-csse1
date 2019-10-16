21615143fe0d4879f87ad55350488b6bbb7666a9
gdm-1718-indymeer <indymeer@student.arteveldehs.be>
2019-10-16 23:57:17 +0200

    init opdracht 3

---
+++ Opdrachten/Opdracht 3/Faculteit/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace opdracht3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            
+          
+            // Factulteit.
+            Console.WriteLine("Geef een getal in:");
+            int getal = Convert.ToInt32(Console.ReadLine());
+            program.Faculteit(getal);
+
+            
+           
+        }
+
+
+       
+        void Faculteit(int getal) {
+            int fac = getal;
+
+            while(getal > 2) {
+                getal--;
+                fac *= getal;
+            }
+
+            Console.WriteLine("Faculteit is: " + fac);
+        }
+    }
+}
---
+++ Opdrachten/Opdracht 3/Fibonacci/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+             // Fibonacci.
+            Console.WriteLine("Geef een eindgetal in:");
+            int eindfib = Convert.ToInt32(Console.ReadLine());
+            program.Fibonacci(eindfib);
+        }
+         void Fibonacci(int getal) {
+            int g0 = 0;
+            int g1 = 1;
+            int next = 0;
+
+            for(int i = 0; i < getal; i++) {
+                if(i == 0) {
+                    next = 0;
+                } else if(i == 1) {
+                    next = 1;
+                } else {
+                    next = g0 + g1;
+                    g0 = g1;
+                    g1 = next;
+                }
+                Console.WriteLine("Fibonacci: " + next);
+            }
+        }
+
+    }
+}
