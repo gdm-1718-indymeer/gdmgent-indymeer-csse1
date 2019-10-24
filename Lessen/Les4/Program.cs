21c3907bb0a6863318318ea9523298bd98016230
gdm-1718-indymeer <indymeer@student.arteveldehs.be>
2019-10-24 14:24:56 +0200

    init

---
+++ Lessen/Les4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Les4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            double fahr = 100;
+            double cels = (fahr - 32) * (5.0 / 9.0);
+
+            Console.WriteLine(cels); 
+
+            Boolean test;
+            test = true; 
+            Console.WriteLine(test.ToString());
+        }
+    }
+}
---
+++ Opdrachten/Opdracht 4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace opdracht4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int cijfer1 = 8;
+            int cijfer2 = 3;
+
+
+            int resultaat1 = Som(cijfer1, cijfer2);
+            Console.WriteLine("De som is: " + resultaat1);
+
+             int resultaat2 = Verschil(cijfer1, cijfer2);
+            Console.WriteLine("Het verschil is: " + resultaat2);
+
+             int resultaat3 = Product(cijfer1, cijfer2);
+            Console.WriteLine("Het product is: " + resultaat3);
+
+             int resultaat4 = Quotient(cijfer1, cijfer2);
+            Console.WriteLine("De deling is: " + resultaat4);
+
+        }
+
+        static int Som(int getal1, int getal2){
+            return getal1 + getal2;
+        }
+        static int Verschil(int getal1, int  getal2)
+        {
+            return getal1 - getal2;
+        }
+        static int Quotient(int getal1, int getal2)
+        {
+            return getal1 / getal2;
+        }
+        static int Product(int getal1, int getal2)
+        {
+            return getal1 * getal2;
+        }
+    }
+}
